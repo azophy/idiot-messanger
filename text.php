2dac889e703ed90852aab196f0e34531ce0116e9
Abdurrahman Shofy Adianto <azophy@gmail.com>
2017-11-10 09:12:29 +0700

    update readme

--- text.php
+++ text.php
@@ -1,5 +1,5 @@
 <script>
 a+=1;
-$('#baaa').text(Math.ceil(a/60).toString().substring(0,5)+ ' Menit ' + Math.ceil(a%60).toString().substring(0,5) + ' detik');
+$('#baaa').text(Math.ceil(a/60).toString().substring(0,5)+ ' Menit ' + Math.ceil(a%60).toString().substring(0,5) + ' Detik');
 </script>
 <span id="baaa"></span> 
